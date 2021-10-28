e0282ace683c88eabce9798089e50a3cb5a46f1d
Sumedha <sumedha@quickwork.co>
2021-10-28 21:45:59 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates");
 
